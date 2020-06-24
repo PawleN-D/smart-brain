86aa505a1569b715f27803714d83b0f1d2e03cdb
PawleN-D <sirpawle@gmail.com>
2020-06-24 17:47:55 +0200

    adjusted the ImageLinkForm appearance with css and classes. Added css file as well

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,9 +6,11 @@ const ImageLinkForm = () => {
             <p className='f3'>
                 {'This Magic Brain will detect faces in your pictures'}
             </p>
-            <div>
-                <input className='f4 pa2 w-70 center' type='tex'/>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            <div className='center'>
+                <div className='center pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='text'/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                </div>
             </div>
         </div>
     )
