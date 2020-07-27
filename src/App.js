8d03eff04cc83b15500a2beda0f1f09d0300a774
PawleN-D <sirpawle@gmail.com>
2020-07-27 15:55:08 +0200

    installed clarifai face detect api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'd6d6c4305b374be7a21dd2ebd593a848'
+});
 
 const particlesOptions = {
   particles: {
@@ -31,7 +36,15 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('click')
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        // do something with response
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   }
 
   render() {
