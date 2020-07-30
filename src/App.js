9551fc9bb8728b23c646027b25c86587aba6288c
PawleN-D <sirpawle@gmail.com>
2020-07-31 01:28:24 +0200

    fixed detect box bug

--- src/App.js
+++ src/App.js
@@ -36,19 +36,21 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    console.log(clarifaiFace);
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.top_row * height),
-      bottomRow: height - (clarifaiFace.top_row * height),
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
     }
   }
 
 displayFaceBox = (box) => {
-  this.setState({box: box})
+  console.log(box);
+  this.setState({box: box});
 }
 
   onInputChange = (event) => {
