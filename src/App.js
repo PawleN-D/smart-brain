01fcc2a10bf8b14d032b4cef530d9ee10b53cd98
PawleN-D <sirpawle@gmail.com>
2020-07-31 04:34:21 +0200

    fixed the sign in routing bug

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -33,6 +34,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
+      isSignedIn: false
     }
   }  
 
@@ -69,6 +71,11 @@ displayFaceBox = (box) => {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
@@ -78,10 +85,9 @@ displayFaceBox = (box) => {
       <Particles className='particles'
         params ={particlesOptions}
       />
-      <Navigation onRouteChange={this.onRouteChange}/>
-      { this.state.route === 'signin'
-        ? <Signin onRouteChange={this.onRouteChange}/>
-        : <div>
+      <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+      { this.state.route === 'home'
+        ? <div>
           <Logo />
             <Rank />
             <ImageLinkForm 
@@ -90,6 +96,13 @@ displayFaceBox = (box) => {
             />
             <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
           </div>
+        : (
+          this.state.route === 'signin'
+          ? <Signin onRouteChange={this.onRouteChange}/>
+          : <Register onRouteChange={this.onRouteChange}/>
+        )
+        
+
       }
       </div>
     );
